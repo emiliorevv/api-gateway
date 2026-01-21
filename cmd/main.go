7fdf402cabb7cb029b0f8747ab6562a63124d9f6
Emilio Revueltas <emilio.revueltas@gmail.com>
2026-01-21 14:32:18 -0600

    added TokenBucketAlgorithm rate limiter

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/emiliorevv/api-gateway/internal/mock"
-
 	"log"
-
+	"net"
 	"net/http"
-
-	"github.com/emiliorevv/api-gateway/internal/proxy"
+	"strings"
 
 	"github.com/emiliorevv/api-gateway/internal/limiter"
+	"github.com/emiliorevv/api-gateway/internal/mock"
+	"github.com/emiliorevv/api-gateway/internal/proxy"
 )
 
 func main() {
@@ -26,17 +25,43 @@ func main() {
 		}
 	}()
 
-	log.Printf("Connection to redis successful")
+	rateLimiter := limiter.NewRateLimiter(rdb, 10, 1.0)
+	log.Println("Rate limiter initialized")
+
+
 
 	backendURL := mock.Run()
 	log.Printf("Backend URL: %s", backendURL)
 
 	proxyHandler, err := proxy.NewReverseProxy(backendURL)
 	if err != nil {
-		log.Fatal("proxy couldnt be created: ", err)
+		log.Fatal("proxy couldn't be created: ", err)
 	}
 
-	http.Handle("/", proxyHandler)
+
+	finalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			ip = strings.Split(r.RemoteAddr, ":")[0]
+		}
+
+		allow, err := rateLimiter.Allow(r.Context(), ip)
+
+		if err != nil{
+			log.Printf("Error on rate limiter: %v", err)
+			proxyHandler.ServeHTTP(w, r)
+			return
+		}
+
+		if !allow{
+			http.Error(w, "Many petitions", http.StatusTooManyRequests)
+			return
+		}
+
+		proxyHandler.ServeHTTP(w,r)
+	})
+
+	http.Handle("/", finalHandler)
 
 	const port = ":8080"
 	log.Printf("Listening on port %s", port)
--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -8,12 +8,67 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const tokenBucketScript = `
+local key = KEYS[1]
+local capacity = tonumber(ARGV[1])
+local rate = tonumber(ARGV[2])
+local now = tonumber(ARGV[3])
+local requested = tonumber(ARGV[4])
+
+local last_tokens = tonumber(redis.call("HGET", key, "tokens"))
+local last_refill = tonumber(redis.call("HGET", key, "last_refill"))
+
+if last_tokens == nil then
+last_tokens = capacity
+last_refill = now
+end
+
+local delta = math.max(0, now - last_refill)
+local filled_tokens = math.min(capacity, last_tokens + (delta * rate))
+
+if filled_tokens < requested then
+return 0 -- False (Blocked)
+end
+
+redis.call("HSET", key, "tokens", filled_tokens - requested, "last_refill", now)
+redis.call("EXPIRE", key, 60)
+return 1 -- True (Accepted)
+`
+
+type RateLimiter struct {
+	client   *redis.Client
+	capacity int
+	rate     float64
+
+
+}
+
+func NewRateLimiter(client *redis.Client, capacity int, rate float64) *RateLimiter {
+	return &RateLimiter{
+		client:   client,
+		capacity: capacity,
+		rate:     rate,
+	}
+}
+
+func (rl *RateLimiter) Allow(ctx context.Context, ip string) (bool, error) {
+	key := fmt.Sprintf("rate_limit_%s", ip)
+
+	now := time.Now().Unix()
+
+	result, err := rl.client.Eval(ctx, tokenBucketScript, []string{key}, rl.capacity, rl.rate, now, 1).Result()
+
+	if err != nil {
+		return false, fmt.Errorf("error executing lua script: %w", err)
+	}
+
+	return result.(int64) == 1, nil
+}
+
 func NewRedisClient(addr string) (*redis.Client, error) {
 
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     addr,
-		Password: "",
-		DB:       0,
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
